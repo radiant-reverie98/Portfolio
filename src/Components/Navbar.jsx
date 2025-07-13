c0e1ffb8ccba102b3a896c3d1579c49797249c58
Tejendra Singh <tejendrambmcse00098@gmail.com>
2025-07-13 14:08:34 +0530

    Added stars animation

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+
+  const navItems = [
+    {name: "Home" , href: "#hero"},
+    {name: "About" , href: "#about"},
+    {name: "Skills" , href: "#skills"},
+    {name: "Projects" , href: "#projects"},
+    {name: "Contact" , href: "#contact"},
+
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar
