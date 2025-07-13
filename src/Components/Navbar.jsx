1075d3f6985f9cdc57c81294b86ef134b4cffa75
Tejendra Singh <tejendrambmcse00098@gmail.com>
2025-07-13 14:43:29 +0530

    Final touches for meteors

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import {cn} from '../lib/utils'
 
 function Navbar() {
 
@@ -10,10 +11,26 @@ function Navbar() {
     {name: "Contact" , href: "#contact"},
 
     ]
+    const [iseScrolled,setIsScrolled] = useState(false)
+
+    
+
+    useEffect(()=>{
+        const handleScroll = () => {
+            setIsScrolled(window.screenY>10)
+        }
+
+        window.addEventListener("scroll",handleScroll)
+    })
   return (
-    <div>
-      
-    </div>
+    <nav className={cn("fixed w-full z-40 transition-all duration-300",
+        iseScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+    )}>
+        <div className="container-flex items-center justify-between">
+            <a></a>
+            
+        </div>
+    </nav>
   )
 }
 
