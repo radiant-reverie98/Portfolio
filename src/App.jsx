a91c2bfeb73231b19b2fd5e403ca743c0d4ebab8
Tejendra Singh <tejendrambmcse00098@gmail.com>
2025-07-13 11:41:30 +0530

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { BrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
 
 
 function App() {
   <BrowserRouter>
     <Routes>
-      <Route index element={} />
+      <Route index element={Home} />
+      <Route path="*" element={NotFound}/>
     </Routes>
   </BrowserRouter>
   
